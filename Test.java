daad334b0523ef7537ea6a8ce545fc42cf94ac16
joshisangeeta <sangeetaj@cybage.com>
2014-04-11 18:22:19 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+class Test
+{
+
+     public static void main(String args[])
+     {
+     
+     
+          System.out.println("Hello ....GIT);
+          
+          
+      }
+      
+      
+}
